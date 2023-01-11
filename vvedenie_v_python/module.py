a5f5f6b4b7ae0d8fc7b3a6fd9663b165e9d943ea
Alexey <lozhkin023@gmail.com>
2023-01-11 13:28:41 +0300

    add_ex_module

---
+++ vvedenie_v_python/module.py
@@ -0,0 +1,6 @@
+from random import randint, randrange
+import math  # Импорт модуля целиком
+import random as rd  # импорт модуля под пвевдоним
+print(round(math.pi, 4))
+print(rd.randint(1, 10))
+print(randint(1, 10))
